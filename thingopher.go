992189eccbd210e6cba93e3a131099690368fc75
Ibsen Ramos-Bonilla <alpha.aleph@gmail.com>
2019-03-23 09:34:47 -0600

    Initial commit

---
+++ thingopher.go
@@ -0,0 +1,9 @@
+// Package thingopher provides a native GUI toolkit for Go.
+package thingopher
+
+/*
+Thinkgopher is a port of the great project written by Robert Bajzat called Thinlet (http://thinlet.sourceforge.net/home.html). This project is being ported, to create a native GUI package for Go.
+*/
+func Thinkgopher() {
+
+}
