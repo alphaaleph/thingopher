d4ee8b7ff581d98972369780b1954b63ab1a89b2
alphaaleph <alpha.aleph@gmail.com>
2019-05-02 19:11:40 -0600

    updated to G naming

--- properties/alignment.go
+++ properties/alignment.go
@@ -1,8 +1,8 @@
 // Package properties defines some of the types used by the widgets parameters.
 package properties
 
-//Alignment defines the various widget horizontal aligments.
-type Alignment uint8
+//GAlignment defines the various widget horizontal aligments.
+type GAlignment uint8
 
 /*
 	ACenter indicates that the text will be centered in the widget.
@@ -10,14 +10,14 @@ type Alignment uint8
 	ARight indicates that the text will be aligned on the right side of the widget.
 */
 const (
-	ACenter Alignment = iota // 0x00
-	ALeft                    // 0x01
-	ARight                   // 0x02
+	ACenter GAlignment = iota // 0x00
+	ALeft                     // 0x01
+	ARight                    // 0x02
 )
 
 //Text returns the string representation of the alignment choice.
-func (alg Alignment) Text() string {
-	switch alg {
+func (gal GAlignment) Text() string {
+	switch gal {
 	case ACenter:
 		return "Center"
 	case ALeft:
@@ -30,6 +30,6 @@ func (alg Alignment) Text() string {
 }
 
 //List returns a slice of alignment strings.
-func (alg Alignment) List() []string {
+func (gal GAlignment) List() []string {
 	return []string{ACenter.Text(), ALeft.Text(), ARight.Text()}
 }
--- properties/category.go
+++ properties/category.go
@@ -1,8 +1,8 @@
 // Package properties defines some of the types used by the widgets parameters.
 package properties
 
-//Category defines the various widget's control and look.
-type Category uint8
+// GCategory defines the various widget's control and look.
+type GCategory uint8
 
 /*
 	CNormal is the default value of a widget's category.
@@ -11,15 +11,15 @@ type Category uint8
 	CLink changes the appereance of a button so that it resembles HTML link.
 */
 const (
-	CNormal  Category = iota // 0x00
-	CDefault                 // 0x01
-	CCancel                  // 0x02
-	CLink                    // 0x03
+	CNormal  GCategory = iota // 0x00
+	CDefault                  // 0x01
+	CCancel                   // 0x02
+	CLink                     // 0x03
 )
 
 //Text returns the string representation of the widget's category.
-func (cat Category) Text() string {
-	switch cat {
+func (gcat GCategory) Text() string {
+	switch gcat {
 	case CNormal:
 		return "Normal"
 	case CDefault:
@@ -34,6 +34,6 @@ func (cat Category) Text() string {
 }
 
 //List returns a slice of category strings.
-func (cat Category) List() []string {
+func (gcat GCategory) List() []string {
 	return []string{CNormal.Text(), CDefault.Text(), CCancel.Text(), CLink.Text()}
 }
