65cb64fbfd5235738538d805719660d90654bbc3
alphaaleph <alpha.aleph@gmail.com>
2019-05-02 21:59:41 -0600

    updated file names, changed to G naming, and set field for better memory layout

--- widgets/display/label.go
+++ widgets/display/label.go
@@ -1,24 +1,44 @@
 // Package display manage all non-input widgets.
 package display
 
+import (
+	p "ebonynaranja.com/thingopher/properties"
+	w "ebonynaranja.com/thingopher/widgets"
+)
+
+// GLabel is the exported implementation of the label widget
+type GLabel interface{}
+
 /*
-Label widget is the representation of a form's label.
+glabel widget is the representation of a form's label.
+
 	Parameters:
-		text		: The text string that the label displays.
+		owner		: Set the component this is labelling. The label will focus the component specified by its name
+						when the mnemonic is activated.
+		component	: name, enabled, visible, tooltip, property, i18n, width, height, colspan, rowspan, weightx,
+						weighty, halign, and valign parameters from component.
 		icon		: The icon image that the label displays.
-		alignment	: The alignment of the label's content along the X axis, vertically is centered. Possible values are: left, center, and right. The default value, if not set, is left. The image position horizontally left, above, and right relative to the text. Default = left.
+		alignment	: The alignment of the label's content along the X axis, vertically is centered. Possible values
+						are: left, center, and right. The default value, if not set, is left. The image position
+						horizontally left, above, and right relative to the text. Default = left.
 		mnemonic	: Specifies the underlined char in the label's text. Default = -1.
-		for_		: Set the component this is labelling. The label will focus the component specified by its name when the mnemonic is activated.
+		text		: The text string that the label displays.
 
-	The following label displays a short text string and an image. Its content is centered both vertically and horizontally.
+	The following label displays a short text string and an image. Its content is centered both vertically and
+	horizontally.
 
-	<label text="Label" icon="image.gif" alignment="center" />
+		<label text="Label" icon="image.gif" alignment="center" />
 */
-type Label struct {
-	text      string
-	icon      Icon
-	alignment Choice
+type glabel struct {
+	owner     *GComponent
+	component w.GComponent
+	icon      p.GIcon
+	alignment p.GAlignment
 	mnemonic  int
-	for_      *Component
-	component Component
+	text      string
+}
+
+// GLabel returns a label object with default values.
+func GLabel() *GLabel {
+	return glabel{alignment: p.ALeft, mnemonic: -1}
 }
---
+++ widgets/display/progressbar.go
@@ -0,0 +1,40 @@
+// Package display manage all non-input widgets.
+package display
+
+import (
+	p "ebonynaranja.com/thingopher/properties"
+	w "ebonynaranja.com/thingopher/widgets"
+)
+
+// GProgressBar is the exported implementation of the progress bar widget
+type GProgressBar interface{}
+
+/*
+gprogressbar widget is the representation of a form's progress bar.
+
+	Parameters:
+		component	: name, enabled, visible, tooltip, property, i18n, width, height, colspan, rowspan, weightx,
+						weighty, halign, and valign parameters from component.
+		orientation	: Possible values are: horizontal, and vertical. Default = horizontal.
+		minimum		: The progressbar's minimum value. Default = 0.
+		maximum		: The progressbar's maximum value. Default = 100.
+		value		: The value is always between the progressbar's minimum and maximum values, inclusive. By default,
+						the value equals the minimum. Default = 0.
+
+	ProressBar displays an integer value within a bounded interval, the following between 25 and 75. Its value is
+	currently in the middle.
+
+		<gprogressbar minimum="25" maximum="75" value="50" orientation="vertical" />
+*/
+type gprogressbar struct {
+	component   w.GComponent
+	orientation p.GOrientation
+	minimum     int
+	maximum     int
+	value       int
+}
+
+// GProgressBar returns a progress bar object with default values.
+func GProgressBar() *GProgressBar {
+	return gprogressbar{orientation: p.Horizontal, maximum: 100}
+}
